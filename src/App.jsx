caa1cf62d912b9c4c9eaea0214f27d576672afee
Øyvind Onarheim <oyvind.aasmul.onarheim@experisacademy.no>
2024-09-26 13:03:19 +0200

    core and extension done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,62 @@
-import Header from './components/Header'
-import initialEmails from './data/emails'
+import { useState } from "react";
+import Header from "./components/Header";
+import initialEmails from "./data/emails";
 
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
   // Use initialEmails for state
-  console.log(initialEmails)
+  console.log(initialEmails);
+  const [hideRead, setHideRead] = useState(false);
+  const [emailState, setEmailState] = useState(initialEmails);
+  const [toggled, setToggled] = useState("");
+  const [hideStarred, setHideStarred] = useState(false);
+
+  function getReadEmails() {
+    let newEmails = initialEmails.filter((x) => x.read !== true);
+    return newEmails;
+  }
+  function getStarredEmails() {
+    let newEmails = initialEmails.filter((x) => x.starred !== true);
+    return newEmails;
+  }
+  function showEmails() {
+    if (hideRead && hideStarred) {
+      return getReadEmails().push(getStarredEmails());
+    } else if (hideRead && !hideStarred) {
+    }
+  }
+  function DisplayEmail(filteredEmails = emailState) {
+    const listEmails = filteredEmails.map((email) => (
+      <li className="email" key={email.id}>
+        <div className="select">
+          <input
+            checked={email.read}
+            onChange={() => {
+              email.read = !email.read;
+              setEmailState([...emailState]);
+            }}
+            className="select-checkbox"
+            type="checkbox"
+          />
+        </div>
+        <div className="star">
+          <input
+            checked={email.starred}
+            onChange={() => {
+              email.starred = !email.starred;
+              setEmailState([...emailState]);
+            }}
+            className="star-checkbox"
+            type="checkbox"
+          />
+        </div>
+        <div className="sender">{email.sender}</div>
+        <div className="title">{email.title}</div>
+      </li>
+    ));
+    return listEmails;
+  }
 
   return (
     <div className="app">
@@ -13,34 +64,47 @@ function App() {
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
-            className="item active"
-            // onClick={() => {}}
+            className={"item " + (toggled === "inbox" ? "active" : "")}
+            onClick={() => {
+              setToggled("inbox");
+            }}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count"></span>
           </li>
           <li
-            className="item"
-            // onClick={() => {}}
+            className={"item " + (toggled === "starred" ? "active" : "")}
+            onClick={() => {
+              setToggled("starred");
+            }}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count"></span>
           </li>
 
-          <li className="item toggle">
-            <label for="hide-read">Hide read</label>
+          <li
+            className={"item " + (toggled === "hideread" ? "active" : "")}
+            onClick={() => {
+              setToggled("hideread");
+            }}
+          >
+            <label htmlFor="hide-read">Hide read</label>
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              checked={hideRead}
+              onChange={() => {
+                setHideRead(!hideRead);
+              }}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {DisplayEmail(hideRead ? getReadEmails() : emailState)}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
